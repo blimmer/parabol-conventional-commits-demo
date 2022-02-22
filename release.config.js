3394e373d239c4c8f5e3ce07641b3fd4614c4d23
Ben Limmer <ben@benlimmer.com>
2022-02-22 15:47:14 -0700

    chore: initial commit

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    ["@semantic-release/commit-analyzer", {
+      preset: "conventionalcommits"
+    }],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+  ]
+}
